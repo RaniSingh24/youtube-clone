1d0b7cb497adb1655cfed9b81cd1459fbc5bbeb2
SINGH Rani <rani.singh@soprasteria.com>
2024-12-01 23:48:29 +0530

    fixed video runnig functionality

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Recommended.css'
 import { useState, useEffect } from 'react';
-import { value_converter } from '../../data';
+import { API_KEY, value_converter } from '../../data';
 import {Link} from 'react-router-dom';
 
 const Recommended = ({categoryId}) => {
